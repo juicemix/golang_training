fcdd892dc9c17c920057680804829cc6863482f5
Rendy Muhardianto 00229 <rendy.muhardianto@corp.bri.co.id>
2019-08-03 12:13:03 +0700

    start session 2

--- main.go
+++ main.go
@@ -5,34 +5,104 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 )
 
-func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Input first name : ")
-	firstName, _ := reader.ReadString('\n')
-	fmt.Print("Input last name : ")
-	lastName, _ := reader.ReadString('\n')
-	fmt.Print("Input delivery distance : ")
-	dist, _ := reader.ReadString('\n')
+var products map[string]Product
+var customers map[string]Customer
+var carts map[string]Cart
 
-	firstName = strings.TrimSuffix(firstName, "\n")
+type Address struct {
+	street, city, region string
+	no                   int
+}
 
-	lastName = strings.TrimSuffix(lastName, "\n")
+type Product struct {
+	id, stock, price int
+	name             string
+}
 
-	fmt.Println("Hello, " + firstName + " " + lastName)
+type Customer struct {
+	firstname, lastname, phone, email string
+	shippingaddress                   Address
+	defaultaddress                    Address
+}
 
-	distInt, _ := strconv.ParseInt(dist, 10, 8)
+type Cart struct {
+	customer Customer
+	item     Product
+	qty      int
+}
 
-	if distInt < 2 {
-		fmt.Println("Shipping : Rp 5000")
-	} else if distInt < 5 {
-		fmt.Println("Shipping : Rp 10000")
-	} else {
-		fmt.Println("Shipping : Rp 15000")
+func calcTotal(price int, qty int) int {
+	return price * qty
+}
+
+func mainMenu() int {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Println("Select Menu : ")
+		fmt.Println("1. Product")
+		fmt.Println("2. Customers")
+		fmt.Println("3. Order")
+		fmt.Println("4. Exit")
+		fmt.Printf("Input : ")
+		scanner.Scan()
+		sel, err := strconv.ParseInt(scanner.Text())
+		if err != nil {
+			if sel != 1 && sel != 2 && sel != 3 && sel != 4 {
+				fmt.Println("Wrong input!")
+			} else {
+				return sel
+			}
+		} else {
+			fmt.Println("Wrong input!")
+		}
 	}
+}
 
-	reader.ReadString('\n')
+func productMenu(products map[string]Product) map[string]Product {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Println("Select Menu : ")
+		fmt.Println("1. View Products")
+		fmt.Println("2. Add Products")
+		fmt.Println("3. Remove Products")
+		fmt.Println("4. Back")
+		fmt.Printf("Input : ")
+		scanner.Scan()
+		sel, err := strconv.ParseInt(scanner.Text())
+		if err != nil {
+			if sel != 1 && sel != 2 && sel != 3 && sel != 4 {
+				fmt.Println("Wrong input!")
+			} else if sel == 1 {
 
+			} else if sel == 2 {
+
+			} else if sel == 3 {
+
+			} else if sel == 4 {
+
+			}
+		} else {
+			fmt.Println("Wrong input!")
+		}
+	}
+}
+
+func main() {
+
+	for {
+		fmt.Println("Welcome!")
+		i := mainMenu()
+		if i == 1 {
+
+		} else if i == 2 {
+
+		} else if i == 3 {
+
+		} else {
+			fmt.Println("Thank you!")
+			return
+		}
+	}
 }
