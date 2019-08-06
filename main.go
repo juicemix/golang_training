a971d492249a4d93016977783f3295cf4c6a0501
Rendy Muhardianto 00229 <rendy.muhardianto@corp.bri.co.id>
2019-08-06 12:59:47 +0700

    done session 2

--- main.go
+++ main.go
@@ -28,9 +28,10 @@ type Customer struct {
 }
 
 type Cart struct {
-	customer Customer
-	item     Product
-	qty      int
+	id            int
+	customer      Customer
+	item          Product
+	qty, subtotal int
 }
 
 func calcTotal(price int, qty int) int {
@@ -60,6 +61,17 @@ func mainMenu() int {
 	}
 }
 
+func viewProducts() {
+	if len(products) == 0 {
+		fmt.Println("There's no products!")
+	} else {
+		fmt.Printf("|ID\t|NAME\t|PRICE\t|STOCK\t|\n")
+		for _, v := range products {
+			fmt.Printf("|%d.\t|%s\t|%d\t|%d\t|\n", v.id, v.name, v.price, v.stock)
+		}
+	}
+}
+
 func productMenu() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -75,14 +87,7 @@ func productMenu() {
 			if sel != 1 && sel != 2 && sel != 3 && sel != 4 {
 				fmt.Println("Wrong input!")
 			} else if sel == 1 {
-				if len(products) == 0 {
-					fmt.Println("There's no products!")
-				} else {
-					fmt.Printf("|ID\t|NAME\t|PRICE\t|STOCK\t|\n")
-					for _, v := range products {
-						fmt.Printf("|%d.\t|%s\t|%d\t|%d\t|\n", v.id, v.name, v.price, v.stock)
-					}
-				}
+				viewProducts()
 			} else if sel == 2 {
 				fmt.Printf("Input ID : ")
 				scanner.Scan()
@@ -118,6 +123,17 @@ func productMenu() {
 	}
 }
 
+func viewCustomers() {
+	if len(customers) == 0 {
+		fmt.Println("There's no products!")
+	} else {
+		fmt.Printf("|NO\t|FIRST NAME\t|LAST NAME\t|PHONE\t|EMAIL\t|DEFAULT ADDRESS\t|SHIPPING ADDRESS\t|\n")
+		for _, v := range customers {
+			fmt.Printf("|%d.\t|%s\t|%s\t|%s\t|%s\t|%s\t|%s\t|\n", v.id, v.firstname, v.lastname, v.phone, v.email, v.defaultaddress.street+" "+v.defaultaddress.no+" "+v.defaultaddress.city+" "+v.defaultaddress.region, v.shippingaddress.street+" "+v.shippingaddress.no+" "+v.shippingaddress.city+" "+v.shippingaddress.region)
+		}
+	}
+}
+
 func customerMenu() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -133,14 +149,7 @@ func customerMenu() {
 			if sel != 1 && sel != 2 && sel != 3 && sel != 4 {
 				fmt.Println("Wrong input!")
 			} else if sel == 1 {
-				if len(customers) == 0 {
-					fmt.Println("There's no products!")
-				} else {
-					fmt.Printf("|NO\t|FIRST NAME\t|LAST NAME\t|PHONE\t|EMAIL\t|DEFAULT ADDRESS\t|SHIPPING ADDRESS\t|\n")
-					for _, v := range customers {
-						fmt.Printf("|%d.\t|%s\t|%s\t|%s\t|%s\t|%s\t|%s\t|\n", v.id, v.firstname, v.lastname, v.phone, v.email, v.defaultaddress.street+" "+v.defaultaddress.no+" "+v.defaultaddress.city+" "+v.defaultaddress.region, v.shippingaddress.street+" "+v.shippingaddress.no+" "+v.shippingaddress.city+" "+v.shippingaddress.region)
-					}
-				}
+				viewCustomers()
 			} else if sel == 2 {
 				fmt.Printf("Input ID : ")
 				scanner.Scan()
@@ -224,6 +233,109 @@ func customerMenu() {
 	}
 }
 
+func cartMenu() {
+
+start:
+	var selected Customer
+	userFound := false
+	scanner := bufio.NewScanner(os.Stdin)
+	viewCustomers()
+	for !userFound {
+
+		fmt.Printf("Select Customer (type \"-1\" to go back) : ")
+		scanner.Scan()
+		user, _ := strconv.Atoi(scanner.Text())
+
+		if user == -1 {
+			return
+		}
+
+		selected, userFound = customers[user]
+
+		if !userFound {
+			fmt.Println("Customer not found")
+		}
+	}
+
+	for {
+		fmt.Printf("Selected Customer : %s %s\n", selected.firstname, selected.lastname)
+		fmt.Println("Select Menu : ")
+		fmt.Println("1. View Cart")
+		fmt.Println("2. Add Cart")
+		fmt.Println("3. Remove Cart")
+		fmt.Println("4. Change Customer")
+		fmt.Println("5. Back")
+		fmt.Printf("Input : ")
+		scanner.Scan()
+		sel, err := strconv.Atoi(scanner.Text())
+		if err == nil {
+			if sel != 1 && sel != 2 && sel != 3 && sel != 4 && sel != 5 {
+				fmt.Println("Wrong input!")
+			} else if sel == 1 {
+				fmt.Println("|NO\t|PRODUCT\t|PRICE\t|QTY\t|SUBTOTAL\t|")
+				for i, v := range carts {
+					if v.customer.id == selected.id {
+						fmt.Printf("|%d\t|%s\t|%d\t|%d\t|%d\t|\n", i, v.item.name, v.item.price, v.qty, v.subtotal)
+					}
+				}
+			} else if sel == 2 {
+				var selectedProduct Product
+				productFound := false
+
+				viewProducts()
+
+				for !productFound {
+					fmt.Printf("Input ID (type \"-1\" to cancel) : ")
+					scanner.Scan()
+					id, _ := strconv.Atoi(scanner.Text())
+					if id == -1 {
+						break
+					}
+					fmt.Printf("Select Product : ")
+					scanner.Scan()
+					product, _ := strconv.Atoi(scanner.Text())
+					fmt.Printf("Input quantity : ")
+					scanner.Scan()
+					qty, _ := strconv.Atoi(scanner.Text())
+
+					selectedProduct, productFound = products[product]
+
+					if !productFound {
+						fmt.Println("Product not found")
+					} else {
+						if qty > selectedProduct.stock {
+							fmt.Println("Insufficent stock")
+						} else {
+							carts[id] = Cart{id, selected, selectedProduct, qty, calcTotal(selectedProduct.price, qty)}
+							selectedProduct.stock = selectedProduct.stock - qty
+							products[selectedProduct.id] = selectedProduct
+							fmt.Println("Success")
+						}
+					}
+				}
+			} else if sel == 3 {
+				fmt.Printf("Input ID : ")
+				scanner.Scan()
+				id, _ := strconv.Atoi(scanner.Text())
+				_, ok := carts[id]
+
+				if ok {
+					delete(carts, id)
+					fmt.Printf("Success")
+				} else {
+					fmt.Printf("ID not found ")
+				}
+			} else if sel == 4 {
+				goto start
+			} else if sel == 5 {
+				return
+			}
+		} else {
+			fmt.Println("Wrong input!")
+		}
+	}
+}
+
 func main() {
 	products = make(map[int]Product)
 	customers = make(map[int]Customer)
@@ -236,7 +348,7 @@ func main() {
 		} else if i == 2 {
 			customerMenu()
 		} else if i == 3 {
-
+			cartMenu()
 		} else {
 			fmt.Println("Thank you!")
 			return
